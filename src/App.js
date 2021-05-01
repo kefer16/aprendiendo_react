1b0b406bca4cd442863a58fa2118bdb73b7c2d97
kefer16 <morafi.1999.16@gmail.com>
2021-05-01 00:31:07 -0500

    creacion de un cursos con estilos EDteam

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from "react";
+import "./styles/styles.scss";
+import Curso from "./Curso";
+const App = () => (
+  <>
+    <div className="main-banner img-container" id="main-banner">
+        <div className="ed-grid lg-grid-6">
+            <div className="lg-cols-4 lg-x-2">
+                <img className="main-banner__img" src="https://picsum.photos/1000/300" alt="imagen" />
+                <div className="main-banner__data s-center">
+                    <p className="t2 s-mb-0">Título del banner</p>
+                    <p> Subtítulo del banner</p>
+                    <a href="http://google.com" className="button">
+                        Botón del banner
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
-  );
-}
 
+    <div className="ed-grid m-grid-3">
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+
+    </div>
+
+    </>
+);
 export default App;
+
+//Reglas
+//1. Toda etiqueta debe cerrarse
+// 2. Los componentes deben devolver
+//un solo elemento Padre
+// 3. Apoyarse de los Fragmenst cuando necesito devolver 2
+//elementos
+// 4. for => HtmlFor
